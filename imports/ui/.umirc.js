31fe15692a2f69526830d7a74cbc9f8bd96369dd
imcoddy <imcoddy@gmail.com>
2018-12-27 15:03:19 +0900

    Update some package setting

--- imports/ui/.umirc.js
+++ imports/ui/.umirc.js
@@ -1,20 +1,24 @@
-
 // ref: https://umijs.org/config/
 export default {
   plugins: [
     // ref: https://umijs.org/plugin/umi-plugin-react.html
-    ['umi-plugin-react', {
-      antd: true,
-      dva: true,
-      dynamicImport: true,
-      title: 'ui',
-      dll: true,
-      hardSource: true,
-      routes: {
-        exclude: [
-          /components/,
-        ],
+    [
+      'umi-plugin-react', {
+        antd: true,
+        dva: true,
+        dynamicImport: true,
+        locale: {
+          default: 'en-US',
+          baseNavigator: true, // 为true时，用navigator.language的值作为默认语言
+          antd: true,
+        }, // 是否启用antd的<LocaleProvider />
+        title: 'ui',
+        dll: true,
+        hardSource: true,
+        routes: {
+          exclude: [/components/,]
+        }
       },
-    }],
-  ],
+    ],
+  ]
 }
