2797f6e3d5481649f07e22d27487ec93ebd096db
imcoddy <imcoddy@gmail.com>
2018-12-27 21:44:07 +0900

    Update packages

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,11 +1,53 @@
 import React from 'react';
-import {DatePicker} from 'antd';
+import { Layout, Menu, Icon } from 'antd';
+import Info from './Info.jsx';
 
 import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
 
+const {
+  Header, Content, Footer, Sider,
+} = Layout;
+
 const App = () => (<div>
-  <h1>Welcome to Meteor!</h1>
-  <DatePicker/>
+  <Layout>
+    <Sider
+      breakpoint="lg"
+      collapsedWidth="0"
+      onBreakpoint={(broken) => { console.log(broken); }}
+      onCollapse={(collapsed, type) => { console.log(collapsed, type); }}
+      >
+      <div className="logo" />
+      <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+        <Menu.Item key="1">
+          <Icon type="user" />
+          <span className="nav-text">nav 1</span>
+        </Menu.Item>
+        <Menu.Item key="2">
+          <Icon type="video-camera" />
+          <span className="nav-text">nav 2</span>
+        </Menu.Item>
+        <Menu.Item key="3">
+          <Icon type="upload" />
+          <span className="nav-text">nav 3</span>
+        </Menu.Item>
+        <Menu.Item key="4">
+          <Icon type="user" />
+          <span className="nav-text">nav 4</span>
+        </Menu.Item>
+      </Menu>
+    </Sider>
+    <Layout>
+      <Header style={{ background: '#fff', padding: 0 }} />
+      <Content style={{ margin: '24px 16px 0' }}>
+        <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
+          content
+        </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>
+        Ant Design ©2018 Created by Ant UED
+      </Footer>
+    </Layout>
+  </Layout>
 </div>);
 
 export default App;
