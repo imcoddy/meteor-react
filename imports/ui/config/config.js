259d15f876a7bf8263fa75710f783a4bd8cadf45
imcoddy <imcoddy@gmail.com>
2018-12-27 17:32:39 +0900

    Update packages

--- imports/ui/config/config.js
+++ imports/ui/config/config.js
@@ -10,14 +10,22 @@ export default {
         locale: {
           default: 'en-US',
           baseNavigator: true, // 为true时，用navigator.language的值作为默认语言
-          antd: true,
+          antd: true
         }, // 是否启用antd的<LocaleProvider />
         title: 'ui',
         dll: true,
         hardSource: true,
         routes: {
           exclude: [/components/,]
-        }
+        },
+      },
+    ],
+    [
+      'umi-plugin-pro-block', {
+        moveMock: false,
+        moveService: false,
+        modifyRequest: true,
+        autoAddMenu: true
       },
     ],
   ]
